66f7e12c7c8c11c938093707d58e7e14955a3fb5
JARJAR1028 <jribando@nyit.edu>
2024-11-09 12:54:28 -0500

    Created restaurant database. Added ability to create new customer

--- Restaurant/src/com/restaurant/Database.java
+++ Restaurant/src/com/restaurant/Database.java
@@ -3,14 +3,13 @@ package com.restaurant;
 import java.sql.Connection;
 import java.sql.DriverManager;
 
-public class Database {
-
-public static Connection connection;
+class Database {
+	public static Connection connection;
 	
 	public static void connect() {
 		try {
 			Class.forName("com.mysql.cj.jdbc.Driver");
-			connection = DriverManager.getConnection("jdbc:mysql://localhost/doctors_office?serverTimezone=EST", "root", "database28");
+			connection = DriverManager.getConnection("jdbc:mysql://localhost/restaurantdb?serverTimezone=EST", "root", "#Jarman28mysql");
 		} catch (Exception e) {
 			System.out.println(e);
 		}
