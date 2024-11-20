232d6e780a54e409b975a5d2f0856e947a0ddadd
jarjar28 <jribando@nyit.edu>
2024-11-19 20:44:53 -0500

    Added UI
    Added Buttons and areas for a customer to make an account

--- Restaurant/src/com/restaurant/CustomerSignIn.java
+++ Restaurant/src/com/restaurant/CustomerSignIn.java
@@ -1,12 +1,17 @@
 package com.restaurant;
 
 import java.awt.EventQueue;
-
+import javax.swing.JTextField;
 import javax.swing.JFrame;
+import javax.swing.JButton;
+import javax.swing.JLabel;
 
 public class CustomerSignIn {
 
 	public JFrame frame;
+	private JTextField userNameTF;
+	private JTextField pswdTF;
+	private JLabel lblNewLabel_1;
 
 	/**
 	 * Launch the application.
@@ -38,6 +43,32 @@ public class CustomerSignIn {
 		frame = new JFrame("Customer Sign in page");
 		frame.setBounds(100, 100, 450, 300);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		
+		userNameTF = new JTextField();
+		userNameTF.setBounds(72, 36, 310, 29);
+		frame.getContentPane().add(userNameTF);
+		userNameTF.setColumns(10);
+		
+		pswdTF = new JTextField();
+		pswdTF.setBounds(72, 94, 310, 29);
+		frame.getContentPane().add(pswdTF);
+		pswdTF.setColumns(10);
+		
+		JButton signInBtn = new JButton("Sign-in");
+		signInBtn.setBounds(72, 134, 310, 29);
+		frame.getContentPane().add(signInBtn);
+		
+		JButton createAccountBtn = new JButton("Create Account");
+		createAccountBtn.setBounds(72, 221, 310, 29);
+		frame.getContentPane().add(createAccountBtn);
+		
+		JLabel usernameLBL = new JLabel("Username");
+		usernameLBL.setBounds(72, 11, 310, 14);
+		frame.getContentPane().add(usernameLBL);
+		
+		JLabel pswdLBL = new JLabel("Password");
+		pswdLBL.setBounds(72, 76, 310, 14);
+		frame.getContentPane().add(pswdLBL);
 	}
-
 }
