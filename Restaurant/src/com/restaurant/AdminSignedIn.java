8450fa3e769c380ed2f8c461f3cfe8853d6564d6
jarjar28 <jribando@nyit.edu>
2024-11-26 20:10:25 -0500

    Added Buttons to AdminSignedIn

--- Restaurant/src/com/restaurant/AdminSignedIn.java
+++ Restaurant/src/com/restaurant/AdminSignedIn.java
@@ -1,8 +1,12 @@
 package com.restaurant;
 
 import java.awt.EventQueue;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.JFrame;
+import javax.swing.UIManager;
+import javax.swing.JButton;
 
 public class AdminSignedIn {
 
@@ -35,9 +39,61 @@ public class AdminSignedIn {
 	 * Initialize the contents of the frame.
 	 */
 	public void initialize() {
-		frame = new JFrame("Employee Sign in page");
+		frame = new JFrame("Admin");
 		frame.setBounds(100, 100, 450, 300);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		setLookAndFeel();
+		manageEmployeesButton();
+		manageCustomersButton();
+		manageMenuButton();
+		manageOrdersButton();
 	}
-
+	
+	public void manageEmployeesButton() {
+		JButton manageEmployeesButton = new JButton("Manage Employees");
+		manageEmployeesButton.setBounds(72, 28, 310, 29);
+		frame.getContentPane().add(manageEmployeesButton);
+		manageEmployeesButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				//goToCustomerSignIn();
+			}
+		});
+	}
+	public void manageCustomersButton() {
+		JButton manageCustomersButton = new JButton("Manage Customers");
+		manageCustomersButton.setBounds(72, 81, 310, 29);
+		frame.getContentPane().add(manageCustomersButton);
+		manageCustomersButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				//goToCustomerSignIn();
+			}
+		});
+	}
+	public void manageMenuButton() {
+		JButton manageMenuButton = new JButton("Manage Menu");
+		manageMenuButton.setBounds(72, 136, 310, 29);
+		frame.getContentPane().add(manageMenuButton);
+		manageMenuButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				//goToCustomerSignIn();
+			}
+		});
+	}
+	public void manageOrdersButton() {
+		JButton manageOrdersButton = new JButton("Manage Reservations/Orders");
+		manageOrdersButton.setBounds(72, 193, 310, 29);
+		frame.getContentPane().add(manageOrdersButton);
+		manageOrdersButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				//goToCustomerSignIn();
+			}
+		});
+	}
+	//Makes the UI look modern if the user is on Windows
+	public void setLookAndFeel() {
+		try {
+			UIManager.setLookAndFeel("com.sun.java.swing.plaf.windows.WindowsLookAndFeel");
+		} catch (Exception e) { }
+	}
 }
